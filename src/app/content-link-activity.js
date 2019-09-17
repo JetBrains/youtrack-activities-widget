0b130f3f667191b329fa5ef18ee154a45c64ee65
Pavel Nikolaev <pavel.nikolaev@jetbrains.com>
2019-09-17 14:20:23 +0200

    JT-54222 reformated link change template

--- src/app/content-link-activity.js
+++ src/app/content-link-activity.js
@@ -1,46 +1,51 @@
 import React from 'react';
+import classNames from 'classnames';
+import Link from '@jetbrains/ring-ui/components/link/link';
 
 import ContentDefaultActivity from './content-default-activity';
-import ActivityIssueInfo from './components/activity-issue-info';
 
 import './style/activities-widget.scss';
+import filter from './activities-filter';
 
 
 class ContentLinkActivity extends ContentDefaultActivity {
 
+  getValueClassName = link => {
+    const valueClass = 'activities-widget__activity__link__change__value';
+    const modClass = link.removed ? `${valueClass}_removed` : `${valueClass}_added`;
+    return classNames(valueClass, modClass);
+  };
+
+  linkToIssue = issue => {
+    const issueId = issue.idReadable;
+    return `${filter.youTrackUrl}/issue/${issueId}`;
+  };
+
   // eslint-disable-next-line react/display-name
   renderContent = activity => {
     const fieldName = activity.field.presentation;
+    const links = activity.removed.map(issue => ({issue, removed: true})).
+      concat(activity.added.map(issue => ({issue, removed: false})));
+
     return (
       <div className="activities-widget__activity__link">
         <div className="activities-widget__activity__link__field-name">
           {`${fieldName}:`}
         </div>
-        <div className="activities-widget__activity__link__value">
-          <div className="activities-widget__activity__link__value__removed">
-            {
-              activity.removed.map(removedLinkIssue =>
-                (
-                  <ActivityIssueInfo
-                    key={removedLinkIssue.id}
-                    issue={removedLinkIssue}
-                  />
-                )
+        <div className="activities-widget__activity__link__change">
+          {
+            links.map(link =>
+              (
+                <Link
+                  key={link.issue.id}
+                  className={this.getValueClassName(link)}
+                  href={this.linkToIssue(link.issue)}
+                >
+                  {link.issue.idReadable}
+                </Link>
               )
-            }
-          </div>
-          <div className="activities-widget__activity__link__value__added">
-            {
-              activity.added.map(addedLinkIssue =>
-                (
-                  <ActivityIssueInfo
-                    key={addedLinkIssue.id}
-                    issue={addedLinkIssue}
-                  />
-                )
-              )
-            }
-          </div>
+            )
+          }
         </div>
       </div>
     );
